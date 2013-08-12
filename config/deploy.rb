c0e061d7479e4e16e0148fa8d3fe6faaa701a689
amite <amit.erandole@gmail.com>
2013-08-12 18:20:45 +0000

    added modified files

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ require "bundler/capistrano"
 set :application, "smallcms"
 set :repository,  "git@github.com:amite/#{application}.git"
 set :user, "vagrant"
-set :deploy_to, "/home/#{user}/apps/#{smallcms}"
+set :deploy_to, "/home/#{user}/apps/#{application}"
 set :use_sudo, false
 
 set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
@@ -13,40 +13,28 @@ server "192.168.1.9", :app, :web, :db, primary: true
 
 default_run_options[:pty] = true
 
-ssh_options[:forward_agent] = true
-
-after "deploy", "deploy:cleanup" # keep only the last 5 releases
-
 namespace :deploy do
-  %w[start stop restart].each do |command|
-    desc "#{command} unicorn server"
-    task command, roles: :app, except: {no_release: true} do
-      run "/etc/init.d/unicorn_#{application} #{command}"
-    end
+  task :start do
+    sudo "service nginx start"
+    sudo "service postgresql start"
+    run "cd #{current_path} && bundle exec unicorn -c config/unicorn.rb -E production -D"
+  end
+  task :stop do
+    sudo "service nginx stop"
+    sudo "service postgresql stop"
+    run "kill `cat /tmp/pids/unicorn.pid`"
   end
 
-  task :setup_config, roles: :app do
-    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
-    run "mkdir -p #{shared_path}/config"
+  task :restart do
+    stop
+    start
   end
-  after "deploy:setup", "deploy:setup_config"
-
-  task :symlink_config, roles: :app do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-  end
-  after "deploy:finalize_update", "deploy:symlink_config"
-
-  desc "Make sure local git is in sync with remote."
-  task :check_revision, roles: :web do
-    unless `git rev-parse HEAD` == `git rev-parse origin/master`
-      puts "WARNING: HEAD is not the same as origin/master"
-      puts "Run `git push` to sync changes."
-      exit
-    end
-  end
-  before "deploy", "deploy:check_revision"
 end
+
+
+# if you want to clean up old releases on each deploy uncomment this:
+after "deploy:restart", "deploy:cleanup"
+
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
