3b8f9b162e90fd28d5cdb905e0d5fe36d4f501eb
amite <amit.erandole@gmail.com>
2013-08-13 19:39:21 +0530

    unmodified default environment

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,9 +9,9 @@ set :ssh_options,     { forward_agent: true }
 set :branch,          "master"
 set :deploy_via, :remote_cache
 
-set :default_environment, {
-  'PATH' => "/home/#{user}/.rbenv/shims:/home/#{user}/.rbenv/bin:$PATH"
-}
+# set :default_environment, {
+#   'PATH' => "/home/#{user}/.rbenv/shims:/home/#{user}/.rbenv/bin:$PATH"
+# }
 
 set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
